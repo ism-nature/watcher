9e0fd57779e1520e882b09729cc26c3fa737a957
os-architect <cnidus@yandex.com>
2019-03-17 17:20:23 -0700

    Reducing number of components

--- watcher/app/routes/index.js
+++ watcher/app/routes/index.js
@@ -1,11 +1,26 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model(){
+    model() {
         return {
-
-            name: "Breno"
-
-        }
+            'id': '198273912',
+            'name': 'Florenas, The Master',
+            'position': {
+                'map': 'Great Hall',
+                'x': '235',
+                'y': '199'
+            },
+            'attributes': {
+                'strength': 76,
+                'agility': 44,
+                'dexterity': 54,
+                'cunning': 32,
+                'vigor': 20
+            },
+            'skills': {
+                'accrobatics': 12,
+                'chemistry': 87
+            }
+        }    
     }
 });
